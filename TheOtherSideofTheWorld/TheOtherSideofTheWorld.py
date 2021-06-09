d7a6c589eb2dec91c2ef231db275682ccfac7f56
kelec <caleb.scarberry@gmail.com>
2021-06-09 16:38:30 -0400

    added more conditions to the latitude ; added a map

--- TheOtherSideofTheWorld/TheOtherSideofTheWorld.py
+++ TheOtherSideofTheWorld/TheOtherSideofTheWorld.py
@@ -1,3 +1,5 @@
+from folium	import	Map
+
 print("Would you like to know the antipode of a place?  (y/n)")
 antipode_start = input()
 
@@ -9,34 +11,19 @@ if antipode_start == str("y"):
 	
 
 	antipode_latitude = latitude.__mul__(int("-1"))
-	if longitude._le_(float("0")):
+	if longitude.__lt__(float("0")):
 		antipode_longitude = longitude.__add__(float("180"))
+	elif longitude.__eq__(float("0")):
+		antipode_longitude = float("180")
+	elif longitude.__gt__(float("180")):
+		antipode_longitude = str("Invalid Longitude")
 	else:
-		antipode_longitude = longitude._sub_(float("180"))
+		antipode_longitude = longitude.__sub__(float("180"))
 
 	print("The antipode of " + str(latitude) + " , " + str(longitude) + " is " + str(antipode_latitude) + " , " + str(antipode_longitude))
-	print()
-	print("Would you like to continue? (y/n)  ")
-	antipode_continue = str(input())
-
-	while antipode_continue == str("y"):
-		print("What is the latitude of the next destination?  ")
-		latitude = float(input())
-		print("What is the longitude of the next destination?  ")
-		longitude = float(input())
-
-		antipode_latitude = latitude.__mul__(int("-1"))
-		if longitude._le_(float("0")):
-			antipode_longitude = longitude.__add__(float("180"))
-		else:
-			antipode_longitude = longitude._sub_(float("180"))
-
-		print("The antipode of " + str(latitude) + " , " + str(longitude) + " is " + str(antipode_latitude) + " , " + str(antipode_longitude))
-		print()
-		print("Would you like to continue? (y/n)  ")
-		antipode_continue = input()
-		if antipode_continue == str("n"):
-			break
+	location = list((antipode_latitude, antipode_longitude))
+	mymap = Map(location)
+	mymap.save(str("antipode.html"))
 elif antipode_start == str("n"):
 	print("Please enjoy your day")
 
