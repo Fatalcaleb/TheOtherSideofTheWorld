7f6dffd062003bba3d5c0190a59da20ff4978538
kelec <caleb.scarberry@gmail.com>
2021-06-09 13:13:18 -0400

    Added a while loop so we can check for multiple destinations instead of restarting the program

--- TheOtherSideofTheWorld/TheOtherSideofTheWorld.py
+++ TheOtherSideofTheWorld/TheOtherSideofTheWorld.py
@@ -1,7 +1,23 @@
-latitude = float(input("What is the latitude of your destination? "))
-longitude = float(input("What is the longitude of your destination? "))
+latitude = float(input("What is the latitude of the destination?  "))
+longitude = float(input("What is the longitude of the destination?  "))
 
 antipode_latitude = latitude.__mul__(int("-1"))
 antipode_longitude = longitude.__mul__(int("-1"))
 
 print("The antipode of " + str(latitude) + " , " + str(longitude) + " is " + str(antipode_latitude) + " , " + str(antipode_longitude))
+print()
+print()
+antipode_continue = str(input("Would you like to continue? (y/n)  "))
+while antipode_continue == str("y"):
+	latitude = float(input("What is the latitude of the next destination?  "))
+	longitude = float(input("What is the longitude of the next destination?  "))
+
+	antipode_latitude = latitude.__mul__(int("-1"))
+	antipode_longitude = longitude.__mul__(int("-1"))
+
+	print("The antipode of " + str(latitude) + " , " + str(longitude) + " is " + str(antipode_latitude) + " , " + str(antipode_longitude))
+	print()
+	print()
+	antipode_continue = input("Would you like to continue? (y/n)  ")
+	if antipode_continue == str("n"):
+		break
