4101dc6847aa592742b4ae5ff2ecfc01eb61d3f8
kelec <caleb.scarberry@gmail.com>
2021-06-09 16:52:21 -0400

    added the map and cleaned up code; had to revert to only looking for one antipode each time program is ran. will be looking at fixing in upcomming builds

--- TheOtherSideofTheWorld/TheOtherSideofTheWorld.py
+++ TheOtherSideofTheWorld/TheOtherSideofTheWorld.py
@@ -3,28 +3,27 @@ from folium	import	Map
 print("Would you like to know the antipode of a place?  (y/n)")
 antipode_start = input()
 
-if antipode_start == str("y"):
+if antipode_start == "y":
 	print("What is the latitude of the destination?  ")
-	latitude = float(input())
+	latitude = input()
 	print("What is the longitude of the destination?  ")
-	longitude = float(input())
+	longitude = input()
 	
 
-	antipode_latitude = latitude.__mul__(int("-1"))
-	if longitude.__lt__(float("0")):
-		antipode_longitude = longitude.__add__(float("180"))
-	elif longitude.__eq__(float("0")):
-		antipode_longitude = float("180")
-	elif longitude.__gt__(float("180")):
+	antipode_latitude = latitude * -1
+	if longitude <= 0:
+		antipode_longitude = longitude + 180
+	
+	elif longitude > 180:
 		antipode_longitude = str("Invalid Longitude")
 	else:
-		antipode_longitude = longitude.__sub__(float("180"))
+		antipode_longitude = longitude - 180
 
 	print("The antipode of " + str(latitude) + " , " + str(longitude) + " is " + str(antipode_latitude) + " , " + str(antipode_longitude))
-	location = list((antipode_latitude, antipode_longitude))
+	location = [antipode_latitude, antipode_longitude]
 	mymap = Map(location)
-	mymap.save(str("antipode.html"))
-elif antipode_start == str("n"):
+	mymap.save("antipode.html")
+elif antipode_start == "n":
 	print("Please enjoy your day")
 
 
