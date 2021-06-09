ae4c6e86251d81dc75d9258fd8a4aabecd7b25b5
kelec <caleb.scarberry@gmail.com>
2021-06-09 13:50:22 -0400

    Added a start  to the program to explaine what we are doing and  change how the inputs are done

--- TheOtherSideofTheWorld/TheOtherSideofTheWorld.py
+++ TheOtherSideofTheWorld/TheOtherSideofTheWorld.py
@@ -1,23 +1,36 @@
-latitude = float(input("What is the latitude of the destination?  "))
-longitude = float(input("What is the longitude of the destination?  "))
+print("Would you like to know the antipode of a place?  (y/n)")
+antipode_start = input()
 
-antipode_latitude = latitude.__mul__(int("-1"))
-antipode_longitude = longitude.__mul__(int("-1"))
-
-print("The antipode of " + str(latitude) + " , " + str(longitude) + " is " + str(antipode_latitude) + " , " + str(antipode_longitude))
-print()
-print()
-antipode_continue = str(input("Would you like to continue? (y/n)  "))
-while antipode_continue == str("y"):
-	latitude = float(input("What is the latitude of the next destination?  "))
-	longitude = float(input("What is the longitude of the next destination?  "))
+if antipode_start == str("y"):
+	print("What is the latitude of the destination?  ")
+	latitude = float(input())
+	print("What is the longitude of the destination?  ")
+	longitude = float(input())
 
 	antipode_latitude = latitude.__mul__(int("-1"))
 	antipode_longitude = longitude.__mul__(int("-1"))
 
 	print("The antipode of " + str(latitude) + " , " + str(longitude) + " is " + str(antipode_latitude) + " , " + str(antipode_longitude))
 	print()
-	print()
-	antipode_continue = input("Would you like to continue? (y/n)  ")
-	if antipode_continue == str("n"):
-		break
+	print("Would you like to continue? (y/n)  ")
+	antipode_continue = str(input())
+
+	while antipode_continue == str("y"):
+		print("What is the latitude of the next destination?  ")
+		latitude = float(input())
+		print("What is the longitude of the next destination?  ")
+		longitude = float(input())
+
+		antipode_latitude = latitude.__mul__(int("-1"))
+		antipode_longitude = longitude.__mul__(int("-1"))
+
+		print("The antipode of " + str(latitude) + " , " + str(longitude) + " is " + str(antipode_latitude) + " , " + str(antipode_longitude))
+		print()
+		print("Would you like to continue? (y/n)  ")
+		antipode_continue = input()
+		if antipode_continue == str("n"):
+			break
+elif antipode_start == str("n"):
+	print("Please enjoy your day")
+
+
